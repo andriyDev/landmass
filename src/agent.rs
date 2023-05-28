ae5a54c3739d31d943fb0ae637a19ce867b32806
andriyDev <andriydzikh@gmail.com>
2023-05-27 20:31:52 -0700

    Add basic accessors for agents.

--- src/agent.rs
+++ src/agent.rs
@@ -9,6 +9,7 @@ pub struct Agent {
   velocity: Vec3,
   radius: f32,
   max_velocity: f32,
+  current_target: Option<Vec3>,
   current_path: Option<Path>,
 }
 
@@ -19,6 +20,33 @@ impl Agent {
     radius: f32,
     max_velocity: f32,
   ) -> Self {
-    Self { position, velocity, radius, max_velocity, current_path: None }
+    Self {
+      position,
+      velocity,
+      radius,
+      max_velocity,
+      current_target: None,
+      current_path: None,
+    }
+  }
+
+  pub fn get_position(&self) -> Vec3 {
+    self.position
+  }
+
+  pub fn get_velocity(&self) -> Vec3 {
+    self.velocity
+  }
+
+  pub fn set_position(&mut self, position: Vec3) {
+    self.position = position;
+  }
+
+  pub fn set_velocity(&mut self, velocity: Vec3) {
+    self.velocity = velocity;
+  }
+
+  pub fn set_target(&mut self, target: Option<Vec3>) {
+    self.current_target = target;
   }
 }
