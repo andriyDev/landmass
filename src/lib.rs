def991f02930a23a4725b6ff61ba0a1bc02776ce
andriyDev <andriydzikh@gmail.com>
2023-06-23 23:44:40 -0700

    Add tests for systems.

--- src/lib.rs
+++ src/lib.rs
@@ -92,6 +92,9 @@ fn update_archipelagos(
   time: Res<Time>,
   mut archipelago_query: Query<&mut Archipelago>,
 ) {
+  if time.delta_seconds() == 0.0 {
+    return;
+  }
   for mut archipelago in archipelago_query.iter_mut() {
     archipelago.archipelago.update(time.delta_seconds());
   }
@@ -241,5 +244,197 @@ fn sync_desired_velocity(
     desired_velocity.0 = glam_vec3_to_bevy_vec3(
       archipelago.get_agent(agent_entity).get_desired_velocity(),
     );
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use bevy::prelude::*;
+  use landmass::NavigationMesh;
+
+  use crate::{
+    Agent, AgentBundle, AgentDesiredVelocity, AgentTarget, Archipelago,
+    ArchipelagoRef, LandmassPlugin,
+  };
+
+  #[test]
+  fn computes_path_for_agent_and_updates_desired_velocity() {
+    let mut app = App::new();
+
+    app
+      .add_plugins(MinimalPlugins)
+      .add_plugin(TransformPlugin)
+      .add_plugin(LandmassPlugin);
+
+    let archipelago_id = app
+      .world
+      .spawn(Archipelago::new({
+        let mut archipelago =
+          landmass::Archipelago::create_from_navigation_mesh(
+            NavigationMesh {
+              mesh_bounds: None,
+              vertices: vec![
+                glam::Vec3::new(1.0, 0.0, 1.0),
+                glam::Vec3::new(4.0, 0.0, 1.0),
+                glam::Vec3::new(4.0, 0.0, 4.0),
+                glam::Vec3::new(3.0, 0.0, 4.0),
+                glam::Vec3::new(3.0, 0.0, 2.0),
+                glam::Vec3::new(1.0, 0.0, 2.0),
+              ],
+              polygons: vec![vec![0, 1, 4, 5], vec![1, 2, 3, 4]],
+            }
+            .validate()
+            .expect("is valid"),
+          );
+        archipelago.agent_options.obstacle_avoidance_margin = 0.0;
+        archipelago
+      }))
+      .id();
+
+    let agent_id = app
+      .world
+      .spawn(TransformBundle {
+        local: Transform::from_translation(Vec3::new(1.5, 0.0, 1.5)),
+        ..Default::default()
+      })
+      .insert(AgentBundle {
+        agent: Agent { radius: 0.5, max_velocity: 1.0 },
+        archipelago_ref: ArchipelagoRef(archipelago_id),
+        target: AgentTarget::Point(Vec3::new(3.5, 0.0, 3.5)),
+        velocity: Default::default(),
+        desired_velocity: Default::default(),
+      })
+      .id();
+
+    // The first update propagates the global transform, and sets the start of
+    // the delta time (in this update, delta time is 0).
+    app.update();
+    // The second update allows landmass to update properly.
+    app.update();
+
+    assert_eq!(
+      app
+        .world
+        .get::<AgentDesiredVelocity>(agent_id)
+        .expect("desired velocity was added")
+        .0,
+      Vec3::new(1.5, 0.0, 0.5).normalize(),
+    );
+  }
+
+  #[test]
+  fn adds_and_removes_agents() {
+    let mut app = App::new();
+
+    app.add_plugins(MinimalPlugins).add_plugin(LandmassPlugin);
+
+    let archipelago_id = app
+      .world
+      .spawn(Archipelago::new(
+        landmass::Archipelago::create_from_navigation_mesh(
+          NavigationMesh {
+            mesh_bounds: None,
+            vertices: vec![],
+            polygons: vec![],
+          }
+          .validate()
+          .expect("is valid"),
+        ),
+      ))
+      .id();
+
+    let agent_id_1 = app
+      .world
+      .spawn(TransformBundle::default())
+      .insert(AgentBundle {
+        agent: Agent { radius: 0.5, max_velocity: 1.0 },
+        archipelago_ref: ArchipelagoRef(archipelago_id),
+        target: AgentTarget::None,
+        velocity: Default::default(),
+        desired_velocity: Default::default(),
+      })
+      .id();
+
+    let agent_id_2 = app
+      .world
+      .spawn(TransformBundle::default())
+      .insert(AgentBundle {
+        agent: Agent { radius: 0.5, max_velocity: 1.0 },
+        archipelago_ref: ArchipelagoRef(archipelago_id),
+        target: AgentTarget::None,
+        velocity: Default::default(),
+        desired_velocity: Default::default(),
+      })
+      .id();
+
+    app.update();
+
+    let archipelago =
+      app.world.get::<Archipelago>(archipelago_id).expect("archipelago exists");
+
+    let mut agent_entities =
+      archipelago.agents.keys().copied().collect::<Vec<_>>();
+    agent_entities.sort();
+    assert_eq!(agent_entities, {
+      let mut expected = [agent_id_1, agent_id_2];
+      expected.sort();
+      expected
+    });
+    assert_eq!(archipelago.archipelago.get_agent_ids().len(), 2);
+
+    let agent_id_3 = app
+      .world
+      .spawn(TransformBundle::default())
+      .insert(AgentBundle {
+        agent: Agent { radius: 0.5, max_velocity: 1.0 },
+        archipelago_ref: ArchipelagoRef(archipelago_id),
+        target: AgentTarget::None,
+        velocity: Default::default(),
+        desired_velocity: Default::default(),
+      })
+      .id();
+
+    app.update();
+
+    let archipelago =
+      app.world.get::<Archipelago>(archipelago_id).expect("archipelago exists");
+
+    let mut agent_entities =
+      archipelago.agents.keys().copied().collect::<Vec<_>>();
+    agent_entities.sort();
+    assert_eq!(agent_entities, {
+      let mut expected = [agent_id_1, agent_id_2, agent_id_3];
+      expected.sort();
+      expected
+    });
+    assert_eq!(archipelago.archipelago.get_agent_ids().len(), 3);
+
+    app.world.despawn(agent_id_2);
+
+    app.update();
+
+    let archipelago =
+      app.world.get::<Archipelago>(archipelago_id).expect("archipelago exists");
+
+    let mut agent_entities =
+      archipelago.agents.keys().copied().collect::<Vec<_>>();
+    agent_entities.sort();
+    assert_eq!(agent_entities, {
+      let mut expected = [agent_id_1, agent_id_3];
+      expected.sort();
+      expected
+    });
+    assert_eq!(archipelago.archipelago.get_agent_ids().len(), 2);
+
+    app.world.despawn(agent_id_1);
+    app.world.despawn(agent_id_3);
+
+    app.update();
+
+    let archipelago =
+      app.world.get::<Archipelago>(archipelago_id).expect("archipelago exists");
+
+    assert_eq!(archipelago.agents.keys().copied().collect::<Vec<_>>(), []);
+    assert_eq!(archipelago.archipelago.get_agent_ids().len(), 0);
   }
 }
