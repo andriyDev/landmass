dc8814f9571ff86ab909f41dfac89489e603a47e
andriyDev <andriydzikh@gmail.com>
2023-06-26 20:09:11 -0700

    Add sample_point to NavigationData.
    This is basically just a pass-through function, but soon it will be more involved.

--- src/nav_data.rs
+++ src/nav_data.rs
@@ -1,5 +1,17 @@
-use crate::ValidNavigationMesh;
+use glam::Vec3;
+
+use crate::{nav_mesh::MeshNodeRef, ValidNavigationMesh};
 
 pub struct NavigationData {
   pub nav_mesh: ValidNavigationMesh,
+}
+
+impl NavigationData {
+  pub fn sample_point(
+    &self,
+    point: Vec3,
+    distance_to_node: f32,
+  ) -> Option<(Vec3, MeshNodeRef)> {
+    self.nav_mesh.sample_point(point, distance_to_node)
+  }
 }
