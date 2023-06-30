076cf3a63d6b1c7d74749be65c74119a2bfec1b3
andriyDev <andriydzikh@gmail.com>
2023-06-30 00:19:54 -0700

    Add tests for sampling points in NavigationData.
    Also fixed bugs with how points were being sampled.

--- src/nav_data.rs
+++ src/nav_data.rs
@@ -39,11 +39,13 @@ impl NavigationData {
         continue;
       }
 
-      let (sampled_point, sampled_node) =
-        match nav_data.nav_mesh.sample_point(point, distance_to_node) {
-          Some(sampled) => sampled,
-          None => continue,
-        };
+      let (sampled_point, sampled_node) = match nav_data
+        .nav_mesh
+        .sample_point(relative_point, distance_to_node)
+      {
+        Some(sampled) => sampled,
+        None => continue,
+      };
 
       let distance = relative_point.distance_squared(sampled_point);
       match best_point {
@@ -54,11 +56,112 @@ impl NavigationData {
       best_point = Some((
         distance,
         (
-          sampled_point,
+          nav_data.transform.apply(sampled_point),
           NodeRef { island_id: *island_id, polygon_index: sampled_node },
         ),
       ));
     }
     best_point.map(|(_, b)| b)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use std::{collections::HashMap, f32::consts::PI};
+
+  use glam::Vec3;
+
+  use crate::{
+    island::Island, nav_data::NodeRef, nav_mesh::NavigationMesh, BoundingBox,
+    Transform,
+  };
+
+  use super::NavigationData;
+
+  #[test]
+  fn samples_points() {
+    let nav_mesh = NavigationMesh {
+      mesh_bounds: None,
+      vertices: vec![
+        Vec3::new(1.0, 1.0, 1.0),
+        Vec3::new(2.0, 1.0, 1.0),
+        Vec3::new(3.0, 1.0, 1.0),
+        Vec3::new(4.0, 1.0, 1.0),
+        Vec3::new(4.0, 1.0, 2.0),
+        Vec3::new(3.0, 1.0, 2.0),
+        Vec3::new(2.0, 1.0, 2.0),
+        Vec3::new(1.0, 1.0, 2.0),
+      ],
+      polygons: vec![vec![0, 1, 6, 7], vec![1, 2, 5, 6], vec![2, 3, 4, 5]],
+    }
+    .validate()
+    .expect("is valid");
+
+    let mut islands = HashMap::new();
+
+    let mut island_1 = Island::new(BoundingBox::new_box(Vec3::ZERO, Vec3::ONE));
+    island_1.set_nav_mesh(
+      Transform { translation: Vec3::ZERO, rotation: 0.0 },
+      nav_mesh.clone(),
+      /* linkable_distance_to_region_edge= */ 0.01,
+    );
+
+    let mut island_2 = Island::new(BoundingBox::new_box(Vec3::ZERO, Vec3::ONE));
+    island_2.set_nav_mesh(
+      Transform { translation: Vec3::new(5.0, 0.1, 0.0), rotation: PI * -0.5 },
+      nav_mesh.clone(),
+      /* linkable_distance_to_region_edge= */ 0.01,
+    );
+
+    islands.insert(1, island_1);
+    islands.insert(2, island_2);
+
+    let nav_data = NavigationData { islands };
+    // Just above island 1 node.
+    assert_eq!(
+      nav_data.sample_point(
+        Vec3::new(1.5, 1.09, 1.5),
+        /* distance_to_node= */ 0.1,
+      ),
+      Some((
+        Vec3::new(1.5, 1.0, 1.5),
+        NodeRef { island_id: 1, polygon_index: 0 }
+      )),
+    );
+    // Just outside island 1 node.
+    assert_eq!(
+      nav_data.sample_point(
+        Vec3::new(0.95, 0.95, 0.95),
+        /* distance_to_node= */ 0.1,
+      ),
+      Some((
+        Vec3::new(1.0, 1.0, 1.0),
+        NodeRef { island_id: 1, polygon_index: 0 }
+      )),
+    );
+    // At overlap, but closer to island 1.
+    assert_eq!(
+      nav_data.sample_point(
+        Vec3::new(3.5, 1.04, 1.5),
+        /* distance_to_node= */ 0.1,
+      ),
+      Some((
+        Vec3::new(3.5, 1.0, 1.5),
+        NodeRef { island_id: 1, polygon_index: 2 }
+      )),
+    );
+    // At overlap, but closer to island 2.
+    assert_eq!(
+      nav_data
+        .sample_point(
+          Vec3::new(3.5, 1.06, 1.5),
+          /* distance_to_node= */ 0.1,
+        )
+        .map(|(p, n)| ((p * 1e6).round() / 1e6, n)),
+      Some((
+        Vec3::new(3.5, 1.1, 1.5),
+        NodeRef { island_id: 2, polygon_index: 0 }
+      )),
+    );
   }
 }
