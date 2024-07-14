bdd70d4baf0f8b679a1c7ff5c335f692e21a0147
andriyDev <andriydzikh@gmail.com>
2024-07-13 22:45:48 -0700

    Make `sample_point` return a `SampledPoint`.
    This doesn't do anything yet, but it allows us to reuse this data for `find_path` (coming soon) and also for returning the "type" of a node (also coming soon).

--- crates/landmass/src/query.rs
+++ crates/landmass/src/query.rs
@@ -1,6 +1,39 @@
+use std::marker::PhantomData;
+
 use thiserror::Error;
 
-use crate::{Archipelago, CoordinateSystem};
+use crate::{nav_data::NodeRef, Archipelago, CoordinateSystem};
+
+/// A point on the navigation meshes.
+pub struct SampledPoint<'archipelago, CS: CoordinateSystem> {
+  /// The point on the navigation meshes.
+  point: CS::Coordinate,
+  /// The node that the point is on.
+  node_ref: NodeRef,
+  /// Marker to prevent this object from out-living a borrow to the
+  /// archipelago.
+  marker: PhantomData<&'archipelago ()>,
+}
+
+// Manual Clone impl for `SampledPoint` to avoid the Clone bound on CS.
+impl<'archipelago, CS: CoordinateSystem> Clone
+  for SampledPoint<'archipelago, CS>
+{
+  fn clone(&self) -> Self {
+    Self {
+      point: self.point.clone(),
+      node_ref: self.node_ref,
+      marker: self.marker,
+    }
+  }
+}
+
+impl<CS: CoordinateSystem> SampledPoint<'_, CS> {
+  /// Gets the point on the navigation meshes.
+  pub fn point(&self) -> CS::Coordinate {
+    self.point.clone()
+  }
+}
 
 /// An error while sampling a point.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Error)]
@@ -17,18 +50,22 @@ pub(crate) fn sample_point<CS: CoordinateSystem>(
   archipelago: &Archipelago<CS>,
   point: CS::Coordinate,
   distance_to_node: f32,
-) -> Result<CS::Coordinate, SamplePointError> {
+) -> Result<SampledPoint<'_, CS>, SamplePointError> {
   if archipelago.nav_data.dirty {
     return Err(SamplePointError::NavDataDirty);
   }
-  let Some((point, _)) = archipelago
+  let Some((point, node_ref)) = archipelago
     .nav_data
     .sample_point(CS::to_landmass(&point), distance_to_node)
   else {
     return Err(SamplePointError::OutOfRange);
   };
 
-  Ok(CS::from_landmass(&point))
+  Ok(SampledPoint {
+    point: CS::from_landmass(&point),
+    node_ref,
+    marker: PhantomData,
+  })
 }
 
 #[cfg(test)]
