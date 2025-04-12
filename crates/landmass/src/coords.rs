bdae3f893f7aa1b90a54594a4c0946b996a9a554
andriyDev <andriydzikh@gmail.com>
2025-04-12 15:33:39 -0700

    Create a trait for defining point sampling distances.
    It would be nice to entirely avoid 2D users from having to deal with anything vertical. By hiding this behind a trait implementation, we can only expose the vertical stuff for 3D users.

--- crates/landmass/src/coords.rs
+++ crates/landmass/src/coords.rs
@@ -6,6 +6,8 @@ use glam::{Vec2, Vec3};
 pub trait CoordinateSystem {
   /// The user-facing coordinate type.
   type Coordinate: Clone;
+  /// The type to use for point sampling options.
+  type SampleDistance: PointSampleDistance;
 
   /// Converts a coordinate in this system to the standard coordinate system.
   fn to_landmass(v: &Self::Coordinate) -> Vec3;
@@ -14,12 +16,46 @@ pub trait CoordinateSystem {
   fn from_landmass(v: &Vec3) -> Self::Coordinate;
 }
 
+/// A configuration of how a type relates to distances when sampling points. See
+/// [`crate::Archipelago::sample_point`] for more.
+pub trait PointSampleDistance {
+  /// The horizontal distance that a node may be sampled. If a sample point is
+  /// further than this distance away horizontally, it will be ignored.
+  fn horizontal_distance(&self) -> f32;
+
+  /// The vertical distance above the query point that a node may be sampled.
+  ///
+  /// If a sample point is further above than this distance, it will be ignored.
+  /// For 2D coordinate systems, this value is not really used, but a value of
+  /// 1.0 is good to avoid floating point errors. This value must be greater
+  /// than negative [`PointSampleDistance::distance_below`] (and preferably
+  /// positive).
+  fn distance_above(&self) -> f32;
+
+  /// The vertical distance below the query point that a node may be sampled.
+  ///
+  /// If a sample point is further below than this distance, it will be ignored.
+  /// For 2D coordinate systems, this value is not really used, but a value of
+  /// 1.0 is good to avoid floating point errors. This value must be less than
+  /// than negative [`PointSampleDistance::distance_above`] (and preferably
+  /// positive).
+  fn distance_below(&self) -> f32;
+
+  /// The ratio between the vertical and the horizontal distances to prefer. For
+  /// example, if this value is 2.0, then a sample point directly below the
+  /// query point 1.9 units away will be selected over a sample point 1.0 unit
+  /// away horizontally. This value must be positive. For 2D coordinate systems,
+  /// the value is irrelevant (so 1.0 is preferred).
+  fn vertical_preference_ratio(&self) -> f32;
+}
+
 /// The standard coordinate system, where X points right, Y points forward, and
 /// Z points up.
 pub struct XYZ;
 
 impl CoordinateSystem for XYZ {
   type Coordinate = Vec3;
+  type SampleDistance = PointSampleDistance3d;
 
   fn to_landmass(v: &Self::Coordinate) -> Vec3 {
     *v
@@ -30,11 +66,53 @@ impl CoordinateSystem for XYZ {
   }
 }
 
+/// A [`PointSampleDistance`] type for 3D coordinate systems.
+#[derive(Debug, PartialEq, Clone)]
+pub struct PointSampleDistance3d {
+  /// The horizontal distance that a node may be sampled. If a sample point is
+  /// further than this distance away horizontally, it will be ignored.
+  pub horizontal_distance: f32,
+
+  /// The vertical distance above the query point that a node may be sampled.
+  ///
+  /// If a sample point is further above than this distance, it will be
+  /// ignored. This value must be greater than [`Self::distance_below`].
+  pub distance_above: f32,
+
+  /// The vertical distance below the query point that a node may be sampled.
+  ///
+  /// If a sample point is further below than this distance, it will be
+  /// ignored. This value must be greater than [`Self::distance_above`].
+  pub distance_below: f32,
+
+  /// The ratio between the vertical and the horizontal distances to prefer.
+  /// For example, if this value is 2.0, then a sample point directly below
+  /// the query point 1.9 units away will be selected over a sample point 1.0
+  /// unit away horizontally. This value must be positive.
+  pub vertical_preference_ratio: f32,
+}
+
+impl PointSampleDistance for PointSampleDistance3d {
+  fn horizontal_distance(&self) -> f32 {
+    self.horizontal_distance
+  }
+  fn distance_above(&self) -> f32 {
+    self.distance_above
+  }
+  fn distance_below(&self) -> f32 {
+    self.distance_below
+  }
+  fn vertical_preference_ratio(&self) -> f32 {
+    self.vertical_preference_ratio
+  }
+}
+
 /// A 2D coordinate system, where X points right, and Y points forward.
 pub struct XY;
 
 impl CoordinateSystem for XY {
   type Coordinate = Vec2;
+  type SampleDistance = f32;
 
   fn to_landmass(v: &Self::Coordinate) -> Vec3 {
     Vec3::new(v.x, v.y, 0.0)
@@ -42,5 +120,23 @@ impl CoordinateSystem for XY {
 
   fn from_landmass(v: &Vec3) -> Self::Coordinate {
     Vec2::new(v.x, v.y)
+  }
+}
+
+impl PointSampleDistance for f32 {
+  fn horizontal_distance(&self) -> f32 {
+    *self
+  }
+
+  fn distance_above(&self) -> f32 {
+    1.0
+  }
+
+  fn distance_below(&self) -> f32 {
+    1.0
+  }
+
+  fn vertical_preference_ratio(&self) -> f32 {
+    1.0
   }
 }
