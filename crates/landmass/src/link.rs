df5c4f57403ce1a042ca57582952f13a2a168a8f
andriyDev <andriydzikh@gmail.com>
2025-08-25 19:31:42 -0700

    Remove off mesh links for deleted animation links and clean up stale portals in animation links.

--- crates/landmass/src/link.rs
+++ crates/landmass/src/link.rs
@@ -1,6 +1,6 @@
 use slotmap::new_key_type;
 
-use crate::CoordinateSystem;
+use crate::{CoordinateSystem, nav_data::NodeRef};
 
 new_key_type! {
   /// The ID of an [`AnimationLink`].
@@ -31,4 +31,29 @@ pub struct AnimationLink<CS: CoordinateSystem> {
 pub(crate) struct AnimationLinkState<CS: CoordinateSystem> {
   /// The link given to us by the user.
   pub(crate) main_link: AnimationLink<CS>,
+  /// The portals that this animation link can be taken from.
+  pub(crate) start_portals: Vec<NodePortal>,
+  /// The portals that this animation link leads to.
+  pub(crate) end_portals: Vec<NodePortal>,
+}
+
+impl<CS: CoordinateSystem> AnimationLinkState<CS> {
+  pub(crate) fn new(link: AnimationLink<CS>) -> Self {
+    Self {
+      main_link: link,
+      start_portals: Default::default(),
+      end_portals: Default::default(),
+    }
+  }
+}
+
+/// A node portal created from a world portal.
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub(crate) struct NodePortal {
+  /// The node that this portal belongs to.
+  pub(crate) node: NodeRef,
+  /// The interval along the original world portal that this node portal takes
+  /// up. The values are always in ascending order, and both are in the range
+  /// [0-1]. This is a fraction along the world portal.
+  pub(crate) interval: (f32, f32),
 }
